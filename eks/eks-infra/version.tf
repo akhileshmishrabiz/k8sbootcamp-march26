703bfcf5fd434845a3dd7729b472b846c90dcf22
Devops with Akhilesh <livingdevops@gmail.com>
2026-05-03 19:44:32 +0530

    oidc test

--- eks/eks-infra/version.tf
+++ eks/eks-infra/version.tf
@@ -8,3 +8,13 @@ terraform {
     }
   }
 }
+
+terraform {
+    backend "s3" {
+        bucket         = "state-bucket-879381241087"
+        key            = "k8sbootcamp-march26/ghoidc/terraform.tfstate"
+        region         = "ap-south-1"
+        encrypt        = true
+        use_lockfile = true
+    }
+}
